8641ca13e6757c785ffa64960e2a13e694ffa911
Ayaan-Singh <asthana.divya@gmail.com>
2024-11-18 17:55:59 +0530

    hi

---
+++ floyd.py
@@ -0,0 +1,10 @@
+print ("half priamid pattern ")
+b = 0
+n = int(input("enter the number of rows"))
+for i in range(n):
+
+    for j in range (i+1):
+     b = b+1
+     print(b, end=" ")
+    print()
+
